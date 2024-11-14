e7cb2280bc664e9b1ef8ffe854566abbb0a03cae
doolot11 <doolot928@gmail.com>
2024-11-15 04:31:48 +0600

    process

--- src/route/allRoute.js
+++ src/route/allRoute.js
@@ -2,7 +2,7 @@ const Router = require("express")
 const router = new Router()
 const Election = require("../controller/index")
 
-router.get("", Election.getTopParties)
+router.get("/top-parties", Election.getTopParties)
 router.get("/get-parties/:city?/:region_id?", Election.getPartiesWithCity)
 // router.get("/uploadcities", Election.uploadCities)
 // router.get("/cities", Election.getCities)
