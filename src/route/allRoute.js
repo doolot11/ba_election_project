d58b7d3002061e6d00ec192bcdbd9ade8ed32ce7
doolot11 <doolot928@gmail.com>
2024-11-16 03:28:27 +0600

    added sort

--- src/route/allRoute.js
+++ src/route/allRoute.js
@@ -7,7 +7,7 @@ router.get("/get-parties/:city?/:region_id?", Election.getPartiesWithCity)
 // router.get("/uploadcities", Election.uploadCities)
 // router.get("/cities", Election.getCities) 
 router.get("/cities/:party?", Election.getCitiesWithParty)
-// router.get("/cities-by-region/:region_id?", Election.getCitiesByRegion)
+router.get("/cities-by-region/:region_id?", Election.getCitiesByRegion)
 
 // module.export = router
 module.exports = router
